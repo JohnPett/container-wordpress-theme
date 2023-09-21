82219b72fcedbdfefe2df14bb509f3664c3266e5
John Pett <john@containerstud.io>
2023-09-21 09:45:35 +0100

    Type changes

--- includes/button.php
+++ includes/button.php
@@ -1,3 +1,3 @@
-<button class="bg-blue-600 text-white px-4 text-sm rounded-full hover:opacity-50 ease-in-out duration-300 font-medium">
+<button class="bg-blue-600 text-white px-4 text-sm rounded-full hover:opacity-50 ease-in-out duration-300 font-bold">
   How to buy
 </button>
