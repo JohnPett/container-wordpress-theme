915d628afa39a2284f448686aa8eb980e7e92ca7
John Pett <john@containerstud.io>
2023-09-21 09:59:23 +0100

    Update menu.php

--- includes/menu.php
+++ includes/menu.php
@@ -1,12 +1,17 @@
 <ul class="flex justify-center content-center flex-wrap">
-  <li class="flex content-center flex-wrap mr-[2vw]">
-    <a href="/about" class="font-bold tracking-wide flex content-center flex-wrap text-sm hover:opacity-50 ease-in-out duration-300 text-gray-700 leading-none">
-      About
-    </a>
-  </li>
-  <li class="flex content-center flex-wrap mr-[2vw]">
-    <a href="/contact" class="font-bold tracking-wide flex content-center flex-wrap text-sm hover:opacity-50 ease-in-out duration-300 text-gray-700 leading-none">
-      Contact
-    </a>
-  </li>
+  <?php
+    $menu_items = wp_get_nav_menu_items('primary-menu');
+    foreach($menu_items as $item) {
+      $title = $item->title;
+      $url = $item->url;
+      $slug = basename($url);
+      $current_slug = the_slug(false);
+      $active = $slug == $current_slug ? 'active' : '';
+      echo '<li class="flex content-center flex-wrap mr-[2vw]">';
+        echo '<a href="'.$url.'" class="font-bold tracking-wide flex content-center flex-wrap text-sm hover:opacity-50 ease-in-out duration-300 text-gray-700 leading-none '.$active.'">';
+          echo $title;
+        echo '</a>';
+      echo '</li>';
+    }
+  ?>
 </ul>
