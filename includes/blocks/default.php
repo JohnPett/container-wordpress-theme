80693e93ac8cf17ca9ed72924d5aaf6a93843b58
John Pett <john@containerstud.io>
2023-09-23 15:07:49 +0100

    Update default.php

--- includes/blocks/default.php
+++ includes/blocks/default.php
@@ -3,7 +3,7 @@
   $text = get_sub_field('text');
   $background = get_sub_field('background');
   $size = 'full';
-  echo '<section class="flex content-center justify-center flex-wrap relative bg-gray-800">';
+  echo '<section class="flex justify-center flex-wrap relative bg-gray-800">';
     if ($background) {
       echo '<img class="w-full h-full object-cover object-center absolute" ';
         echo acf_responsive_image($background['ID'], $size, '1024px');
